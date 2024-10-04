75d4ccd58522b73cdaa265dcfaf3df70c56eaeb3
Dusty White <dusty.white@gmail.com>
2024-10-03 20:34:31 -0500

    Update testFittedModel.py

--- tests/testFittedModel.py
+++ tests/testFittedModel.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import sklearn
 import statsmodels.discrete.discrete_model as dm
+import re
 
 # Import your code from parent directory
 import os
@@ -19,13 +20,16 @@ from assignment2 import model, modelFit, pred
 
 # Run the checks
 
+with open("assignment2.py") as f:
+  test = f.read()
+
 class testCases(unittest.TestCase):
     def testFittedModel(self):
         if hasattr(modelFit, 'tree_'):
             self.assertTrue(True)
         elif hasattr(modelFit, 'coef_'):
             self.assertTrue(True)
-        elif hasattr(modelFit, 'coef_'):
+        elif re.search(r"modelFit[ ]?=[ ]?model.fit\(", test):
             self.assertTrue(True)
         elif hasattr(model, '_Booster'):
             self.assertTrue(True)
